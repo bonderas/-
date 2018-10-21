8b5d28584cf9af79086937b72b9519f5a913d647
bonderas <bonderas84@mail.ru>
2018-10-21 22:23:39 +0300

    Test1(исправления)
    Test1(исправления)

--- MainClass.java
+++ MainClass.java
@@ -1,4 +1,7 @@
 public class MainClass
 {
-
+    public int getLocalNumber()
+    {
+        return 14;
+    }
 }
--- MainClassTest.java
+++ MainClassTest.java
@@ -3,5 +3,9 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
-
+    @Test
+    public void testGetLocalNumber() {
+        int a = this.getLocalNumber();
+        Assert.assertTrue( "Метод НЕ возвращает число 14",a == 14);
+    }
 }
