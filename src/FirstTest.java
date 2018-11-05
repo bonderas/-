faf9a0d8481571d8cb3e20e11ec9971a34dd8e4c
bonderas <bonderas84@mail.ru>
2018-11-05 21:44:24 +0300

    Ex2: Создание метода

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -30,9 +35,43 @@ public class FirstTest {
     {
         driver.quit();
     }
+
     @Test
-    public void firstTest()
+    public void existWordSearch()
     {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+        WebElement search_element = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Не найдена строка поиска в Вики",
+                15
+        );
+        String seach_word = search_element.getAttribute("text");
+        Assert.assertEquals(
+                "В строке поиска в Вики , нет слова Search",
+                "Search…",
+                seach_word
+
+        );
+
     }
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+
 }
